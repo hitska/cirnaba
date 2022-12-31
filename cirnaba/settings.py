59dbefd4fa55dde520d667dfc984f86fa7f545ec
hitska <gobbler.kun@gmail.com>
2022-12-31 19:33:29 +0300

    Добавил фавикон вакабы

--- cirnaba/settings.py
+++ cirnaba/settings.py
@@ -115,8 +115,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+# Внутри каждого приложения.
 STATIC_URL = 'static/'
+# Внутри ROOT-директории.
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
